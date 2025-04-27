a0a75d89676f9901b282224b4c2886ba8b2b54cb
Jynra <jynrok@proton.me>
2025-04-27 12:34:57 +0200

    awf

--- proxy.py
+++ proxy.py
@@ -13,7 +13,7 @@ from http import HTTPStatus
 
 # Configuration
 PORT = 9425
-OLLAMA_URL = "http://localhost:11434/api/generate"  # URL vers Ollama sur le réseau Docker
+OLLAMA_URL = "http://host.docker.internal:11434"  # URL vers Ollama sur le réseau Docker
 
 class ProxyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_OPTIONS(self):
